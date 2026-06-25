fa9cd4529851eff66579984856edb710f80581fe
Sama_Mohamed <samamohamed4131@gmail.com>
2026-06-25 17:14:26 +0300

    Update main.py

--- Backend/main.py
+++ Backend/main.py
@@ -1,26 +1,26 @@
 from fastapi import FastAPI , HTTPException , status
 from typing import List
-from database import MEDICINES_DB
+from database import get_db
 from schemas import MedicienCreate , MedicienResponse
 
 app= FastAPI()
 
 @app.get("/medicines",response_model=list[MedicienResponse])
 def get_all_mediciens():
-    return list(MEDICINES_DB.values())
+    return list(get_db.values())
 
 @app.get("/medicines/{medicine_id}",response_model=MedicienResponse)
 def get_medicine_by_id(medicine_id:int):
-    if  medicine_id not  in MEDICINES_DB:
+    if  medicine_id not  in get_db:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f" Medicine with id {medicine_id} was not found in our records"
         )
-    return MEDICINES_DB[medicine_id]
+    return get_db[medicine_id]
 
 @app.post("/medicines",response_model=MedicienResponse, status_code=status.HTTP_201_CREATED)
 def create_medicien(payload: MedicienCreate):
-    new_id=max(MEDICINES_DB.keys(), default=0) + 1
+    new_id=max(get_db.keys(), default=0) + 1
     new_medicien={
         "id": new_id,
         "name": payload.name,
@@ -28,29 +28,29 @@ def create_medicien(payload: MedicienCreate):
         "in_stock": True
     }
     
-    MEDICINES_DB[new_id] = new_medicien
+    get_db[new_id] = new_medicien
     return new_medicien
 
 @app.put ("/medicines/{medicien_id}",response_model=MedicienResponse)
 def update_medicien(medicien_id: int, update_data:MedicienCreate):
-    if medicien_id not in MEDICINES_DB:
+    if medicien_id not in get_db:
         raise HTTPException(status_code=404 , detail="Medicien not Found")
     
     update_record={
         "id":medicien_id,
         "name": update_data.name,
         "price": update_data.price,
-        "in_stock": MEDICINES_DB[medicien_id]
+        "in_stock": get_db[medicien_id]
     }
     
-    MEDICINES_DB[medicien_id]= update_record
+    get_db[medicien_id]= update_record
     return update_record
 
 
 @app.delete("/medicines/{medicien_id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_medicien(medicien_id: int):
-    if medicien_id not in MEDICINES_DB:
+    if medicien_id not in get_db:
         raise HTTPException(status_code=404 , detail="Medicien not Found")
     
-    del MEDICINES_DB[medicien_id]
+    del get_db[medicien_id]
     return None
